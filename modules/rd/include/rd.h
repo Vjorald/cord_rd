5cb5f198d82db55a34c0d074eeb35d49578b7992
Vjorald <vjoraldvlusha@live.com>
2024-12-05 01:49:50 +0100

    Carry out the initial separation of concerns

--- modules/rd/include/rd.h
+++ modules/rd/include/rd.h
@@ -1,3 +1,42 @@
+#define BASE_URI_MAX_LEN 64
+#define ENDPOINT_NAME_MAX_LEN 50
+#define RESOURCES_MAX_LEN 100
+#define REGISTERED_ENDPOINTS_MAX_NUMBER 100
+#define DELETED_ENDPOINTS_MAX_NUMBER 100
+#define LOOKUP_RESULTS_MAX_LEN 100
+#define INITIAL_NUMBER_REGISTERED_ENDPOINTS 0
+#define INITIAL_NUMBER_DELETED_ENDPOINTS 0
+#define LOCATION_STR_1_MAX_LEN 6
+#define LOCATION_STR_2_MAX_LEN 2
+#define LOCATION_NUMBER_STR_MAX_LEN 4
+#define LOCATION_STR_MAX_LEN 12
+#define RESOURCE_URI_MAX_NUMBER 10
+#define RESOURCE_URI_MAX_LEN 100
+#define QUERY_BUFFER_MAX_LEN 100
+#define LIFETIME_STR_MAX_LEN 10
+#define LOOKUP_RESULT_STR_MAX_LEN 1024
+
+typedef struct nodeelement{
+    char base[BASE_URI_MAX_LEN];
+    char name[ENDPOINT_NAME_MAX_LEN];
+    int lt;
+    char ressources[RESOURCES_MAX_LEN];
+    intrusive_list_node node_management;
+} Endpoint;
+
+
+extern intrusive_list_node *head;
+
+extern Endpoint list[REGISTERED_ENDPOINTS_MAX_NUMBER];
+
+extern Endpoint deleted_registrations_list[DELETED_ENDPOINTS_MAX_NUMBER];
+
+extern Endpoint lookup_result_list[LOOKUP_RESULTS_MAX_LEN];
+
+extern int number_registered_endpoints;
+
+extern int number_deleted_registrations;
+
 extern u_int8_t _req_buf[CONFIG_GCOAP_PDU_BUF_SIZE];
 
 extern sock_udp_t sock;
@@ -8,3 +47,31 @@ extern int location_epsim_endpoint;
 
 
 void resource_directory_init(void);
+
+void parse_query_buffer(unsigned char *query_buffer, char *ep, char *lt);
+
+void build_location_string(int location_nr, char* location_str);
+
+void build_base_uri_string(char* addr_str, char* base_uri);
+
+void find_endpoints_by_pattern(char* pattern);
+
+Endpoint find_endpoint_by_pattern(char* pattern);
+
+void initialize_endpoint(char *lifetime, char *endpoint_name, Endpoint *endpoint_ptr, intrusive_list_node *node_ptr, char *base_uri, coap_pkt_t *pdu, char* location_str, int location_nr);
+
+int get_next_empty_location(Endpoint* deleted_list);
+
+int get_previous_endpoint_location(int actual_location);
+
+int extract_number_from_location(char *path);
+
+void extract_value_from_query(const char *input, char *href_value, char* pref);
+
+void build_result_string(char* lookup_result, char* first_bracket, char* second_href_bracket, char* ep_key, char* base, char* rt, Endpoint* endpoint);
+
+void build_resource_string(int number_sensors, char extracted_sensor_uris[RESOURCE_URI_MAX_NUMBER][RESOURCE_URI_MAX_LEN], char* lookup_result, Endpoint* endpoint);
+
+int extract_resource_uris(const char *input, char uris[RESOURCE_URI_MAX_NUMBER][RESOURCE_URI_MAX_LEN]);
+
+int printList(Endpoint* endpoint);
